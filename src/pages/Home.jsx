d2bb1bbd9b1c1134a220407d02edbc8782746a20
Kevin Wu <kingonyt4@gmail.com>
2022-02-16 15:36:10 -0800

    Updated Farming

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import './../../src/App.css'
+import React from "react";
+import "./../../src/home.css";
 
 function Home() {
-    return ( 
-        <div className="home">
-            <div className="title">
-            <h1>Acient Mesopotamia</h1>
-            <button><a href="/history">Get Started</a></button>
-
-            </div>
-
-        </div>
-
-        
-     );
+  return (
+    <div className="home">
+      <div className="title">
+        <h1>Acient Mesopotamia</h1>
+        <button>
+          <a href="/history">Get Started</a>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
